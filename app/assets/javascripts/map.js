4212316059fcf9b8b2d45fc1dd4f83bf7951c2d2
jeffslutzky <jeffslutzky@gmail.com>
2016-02-11 17:02:43 -0500

    display basic US map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,4 +1,26 @@
 $(function() {
-  console.log('hi');
-  
+
+  var w = 1300;
+  var h = 700;
+
+  var projection = d3.geo.albersUsa()
+      .translate([w/2, h/2])
+      .scale([1300]);
+
+  var path = d3.geo.path()
+      .projection(projection);
+
+  var svg = d3.select(".container")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h);
+
+  d3.json("us-states.json", function(json) {
+    svg.selectAll("path")
+        .data(json.features)
+        .enter()
+        .append("path")
+        .attr("d", path);
+  })
+
 })
