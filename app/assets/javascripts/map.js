e4d89399bf1af50f1ba6ce1eadc84d527c3334b9
jeffslutzky <jeffslutzky@gmail.com>
2016-02-13 17:35:01 -0500

    implement right-click

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -10,7 +10,7 @@ $(function() {
   var path = d3.geo.path()
       .projection(projection);
 
-  var svg = d3.select(".container")
+  var svg = d3.select(".map")
       .append("svg")
       .attr("width", w)
       .attr("height", h);
@@ -23,7 +23,8 @@ $(function() {
         .attr("d", path)
         .classed("neutral", true)
         .attr("id", "states")
-        .on("click", click);
+        .on("click", click)
+        .on("contextmenu", rightClick);
     svg.selectAll("text")
         .data(json.features)
         .enter()
@@ -40,6 +41,32 @@ $(function() {
   var blueEV = 0;
   var redEV = 0;
   var noEV = 538;
+
+  function rightClick(d) {
+    if (this.classList == "neutral") {
+      d3.select(this)
+          .classed({"neutral": false, "red": true});
+      redStates.push(d.properties.name);
+      redEV += d.ev;
+      noEV -= d.ev;
+    } else if (this.classList == "blue") {
+      d3.select(this)
+          .classed({"blue": false, "neutral": true});
+      blueStates.splice($.inArray(d.properties.name, blueStates),1);
+      blueEV -= d.ev;
+      // redStates.push(d.properties.name);
+      noEV += d.ev;
+    } else {
+      d3.select(this)
+          .classed({"red": false, "blue": true});
+      redStates.splice($.inArray(d.properties.name, redStates),1);
+      redEV -= d.ev;
+      blueEV += d.ev;
+    };
+    $("#dems").text(blueEV);
+    $("#reps").text(redEV);
+    $("#unassigned").text(noEV);
+  };
 
   function click(d) {
     if (this.classList == "neutral") {
