4588e492ec8dd4341d4e32166f3cea2291122383
jeffslutzky <jeffslutzky@gmail.com>
2016-07-04 20:06:42 -0400

    Work on sessions and button styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:name])
     if user && user.authenticate(params[:password])
       log_in(user)
-      redirect_to user_path(current_user.id)
+      redirect_to root_path(current_user.id)
     else
       flash.now[:alert] = "We don't seem to know that user. Please try logging in again, or click \"Sign up\" if you are a new user."
       render 'new'
