3fe776ec76536a56df761608d6bcb6aeb55a8c0d
jeffslutzky <jeffslutzky@gmail.com>
2016-07-30 13:25:12 -0400

    Don't let a user save/update another user's map

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,7 +13,7 @@ class SessionsController < ApplicationController
       log_in(user)
       redirect_to root_path(current_user.id)
     else
-      flash.now[:alert] = "We don't seem to know that user. Please try logging in again, or click \"Sign up\" if you are a new user."
+      flash.now[:alert] = "I don't seem to know that user. Please try logging in again, or click \"Sign up\" if you are a new user."
       render 'new'
     end
   end
